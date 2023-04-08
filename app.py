48bf83818774210cfc71fe39c85a07ea1f3d55c1
Sudheer <sudheerbilager23@gmail.com>
2023-04-08 15:46:37 +0530

    updated

--- app.py
+++ app.py
@@ -8,40 +8,74 @@ import pandas as pd
 import numpy as N
 import requests
 import json
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 train_data = pd.read_excel('E:\SUDHEER\Major project\Data_Train.xlsx')
 
-data = pd.DataFrame(train_data,columns=['Source','Destination','Price','Airline'])
+data = pd.DataFrame(train_data,columns=['Source','Destination','Price','Airline','Dep_Time','Arrival_Time','Duration'])
 source=data['Source'].tolist()
 price=data['Price'].tolist()
 dest=data['Destination'].tolist()
 air=data['Airline'].tolist()
+Dep_Time_list=data['Dep_Time'].tolist()
+Arrival_Time_list=data['Arrival_Time'].tolist()
+Duration=data['Duration'].tolist()
+
 res = N.array(air) 
 unique_res = N.unique(res) 
 
-un=N.unique(N.array(source))
+S_cityname=N.unique(N.array(source))
 dun=N.unique(N.array(dest))
-print(un)
+
+Dep_Time = [x for x in Dep_Time_list if len(x) <= 5]
+Arrival_Time = [x for x in Arrival_Time_list if len(x) <= 5]
+
+
+
+url = 'https://www.fabhotels.com/blog/tourist-destinations-in-india/'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+h3_name=soup.find_all('h3')
+
+h3_list=[]
+for h3link in h3_name:
+    h3_list.append(h3link.text)
+
+img_tag = soup.find_all('img')
+links=[]
+
+for img_link in img_tag:
+    links.append(img_link.get('src'))
+
+print(h3_list)
+
 price_del=[]
 params = {
   'api_key': '02861096-f9da-4c17-888e-71b2af1011eb',
   'country_code':'IN'
   
 }
-method = 'suggest?q=mum'
+method = 'airports'
 api_base = 'http://airlabs.co/api/v9/'
 api_result = requests.get(api_base+method, params)
 api_response = api_result.json()['response']
+cityname=[]
+for d in api_response:
+    name=d['name']
+    cityname.append(name)
 
-
-City=['Delhi',"Mumbai","Chennai","Kolkata","Banglore"]
-
+print(price[5])
+City=['DEL-Delhi',"BOM-Mumbai","MAA-Chennai","CCU-Kolkata","BLR-Banglore"]
+link='https://tickets.paytm.com/flights/flightSearch//BOM-Mumbai/1/0/0/E/2023-04-06'
 @app.route("/")
 @cross_origin()
 def home():
     
-    return render_template("home.html",City=un,Desti=dun)
+    return render_template("home.html",City=S_cityname,Desti=dun,price=price,links=links,h3_list=h3_list)
 del_p={}
 
 
@@ -53,13 +87,7 @@ del_p={}
 def predict():
    
     if request.method == "POST":
-        
-      
-        
-
-
-
-
+       
         # Date_of_Journey
         date_dep = request.form["Dep_Time"]
         Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
@@ -73,25 +101,9 @@ def predict():
 
         # Arrival
         date_arr = request.form["Arrival_Time"]
-        Arrival_hour = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").hour)
-        Arrival_min = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").minute)
         # print("Arrival : ", Arrival_hour, Arrival_min)
 
-        # Duration
-        dur_hour = abs(Arrival_hour - Dep_hour)
-        dur_min = abs(Arrival_min - Dep_min)
-        # print("Duration : ", dur_hour, dur_min)
-
-        # Total Stops
-       
-        # print(Total_stops)
-
-        # Airline
-        # AIR ASIA = 0 (not in column)
-       
-       
-
-      
+        # Duration    
         Source = request.form["Source"]
 
     
@@ -112,7 +124,7 @@ def predict():
     
 
        
-        return render_template('result.html',City=un,Desti=dun,sorted_dict=sorted_dict,Source=request.form["Source"],Destination=request.form["Destination"],Date=request.form["Dep_Time"])
+        return render_template('result.html',T_Dep=Dep_Time,T_arr=Arrival_Time,Duration=Duration, City=S_cityname,Desti=dun,sorted_dict=sorted_dict,Source=request.form["Source"],Destination=request.form["Destination"],Date=request.form["Dep_Time"])
     
 
 
@@ -138,7 +150,7 @@ def search():
     
 
         
-        return render_template('result.html',City=un,Desti=dun,sorted_dict=sorted_dict,Source=request.form["source_input"],Destination=request.form["source_out"],Date=request.form["source_date"])
+        return render_template('result.html',T_Dep=Dep_Time,T_arr=Arrival_Time,Duration=Duration,City=S_cityname,Desti=dun,sorted_dict=sorted_dict,Source=request.form["source_input"],Destination=request.form["source_out"],Date=request.form["source_date"])
     
 
 
